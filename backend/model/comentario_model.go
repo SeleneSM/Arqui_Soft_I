a72fec2e88297f9520a3de85c752aad7222490b7
laru <larape715@gmail.com>
2024-07-09 13:53:49 -0300

    inicio comentarios

---
+++ backend/model/comentario_model.go
@@ -0,0 +1,15 @@
+package model
+
+import (
+	"time"
+)
+
+type Comentario struct {
+	ID               int       `gorm:"primaryKey"`
+	ID_usuario       int       `gorm:"not null"`
+	ID_curso         int       `gorm:"not null"`
+	Usuario          User      `gorm:"foreignKey:UsuarioID;references:ID"`
+	Fecha_comentario time.Time `gorm:"type:date"`
+}
+
+type Comentarios []Inscripcion
